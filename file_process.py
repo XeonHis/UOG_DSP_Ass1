0809ebe933bc956123f3be7cabb854174a054373
Paul_Win <yingheting@gmail.com>
2021-10-10 20:52:40 +0100

    sliced time and normalized wave data

--- file_process.py
+++ file_process.py
@@ -16,8 +16,15 @@ def show_time_domain(file_path):
     wave_data = np.frombuffer(data, dtype=np.int32)
     time = np.arange(0, nframes) / framerate
     max_wave = wave_data.max()
+    normalized_wave_data = wave_data / max_wave
+    time_length = len(time)
+    normalized_wave_data_length = len(normalized_wave_data)
+    # sliced time & normalized wave data
+    sliced_time = time[int(0.5 * time_length):int(0.9 * time_length)]
+    sliced_normalized_wave_data = normalized_wave_data[int(0.5 * normalized_wave_data_length):int(0.9 * normalized_wave_data_length)]
 
-    plt.plot(time, wave_data / max_wave)
+    plt.plot(time, normalized_wave_data)
+    # plt.plot(sliced_time, sliced_normalized_wave_data)
     plt.title('Time Domain')
     plt.xlabel('Time')
     plt.ylabel('Normalised amplitudes')

3934f011052b60706430882fc600b8aa726cd92c
c712g285 <54552684+c712g285@users.noreply.github.com>
2021-10-12 11:13:56 +0100

    g1

--- file_process.py
+++ file_process.py
@@ -16,12 +16,12 @@ def show_time_domain(file_path):
     wave_data = np.frombuffer(data, dtype=np.int32)
     time = np.arange(0, nframes) / framerate
     max_wave = wave_data.max()
-
-    plt.plot(time, wave_data / max_wave)
-    plt.title('Time Domain')
-    plt.xlabel('Time')
-    plt.ylabel('Normalised amplitudes')
-    plt.show()
+    return time, wave_data/max_wave
+    # plt.plot(time, wave_data / max_wave)
+    # plt.title('Time Domain')
+    # plt.xlabel('Time')
+    # plt.ylabel('Normalised amplitudes')
+    # plt.show()
 
 
 def show_freq_domain(file_path):
@@ -38,14 +38,26 @@ def show_freq_domain(file_path):
     fft = np.fft.fft(wave_data)
     abs_fft = np.abs(fft)
     freqs = np.linspace(0, framerate / 2, nframes)
+    return np.log(freqs + epsilon), np.log(abs_fft + epsilon)
+    # plt.plot(np.log(freqs + epsilon), np.log(abs_fft + epsilon))
+    # plt.title('Frequency Domain')
+    # plt.xlabel('Frequency')
+    # plt.ylabel('')
+    # plt.show()
 
-    plt.plot(np.log(freqs + epsilon), np.log(abs_fft + epsilon))
+
+if __name__ == '__main__':
+    x_time,y_time = show_time_domain('asset/vowel_A.wav')
+    x_freq,y_freq = show_freq_domain('asset/vowel_A.wav')
+    plt.figure(figsize=(80,40))
+    plt.subplot(2,1,1)
+    plt.plot(x_time, y_time)
+    plt.title('Time Domain')
+    plt.xlabel('Time')
+    plt.ylabel('Normalised amplitudes')
+    plt.subplot(2,1,2)
+    plt.plot(x_freq, y_freq)
     plt.title('Frequency Domain')
     plt.xlabel('Frequency')
     plt.ylabel('')
     plt.show()
-
-
-if __name__ == '__main__':
-    show_time_domain('asset/sound2.wav')
-    # show_freq_domain('asset/sound1.wav')

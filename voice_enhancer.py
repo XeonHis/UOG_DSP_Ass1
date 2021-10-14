6d350f47a961a6f0c2374f057be0b156e1619f07
vincent972123 <392025574@qq.com>
2021-10-15 00:39:23 +0100

    enhance

--- voice_enhancer.py
+++ voice_enhancer.py
@@ -19,10 +19,8 @@ def enhance(x_freq, start_freq, end_freq):
     
     wave_data, nchannels, sample_width, framerate, numframes = read_file('asset/new_record/newhappy.wav')
     wave_data_fre = np.fft.fft(wave_data)
-
     start = bounds[0]
     end = bounds[-1]
-
     wave_data_fre[start:end] = wave_data_fre[start:end] * 10
     wave_data_fre[int(len(wave_data_fre)-end):int(len(wave_data_fre)-start)] = wave_data_fre[int(len(wave_data_fre)-end):int(len(wave_data_fre)-start)]*10
     enhance = np.fft.ifft(wave_data_fre)
@@ -33,34 +31,6 @@ def enhance(x_freq, start_freq, end_freq):
    
 if __name__ == '__main__':
     
-    x_time, y_time = show_time_domain('asset/new_record/newhappy.wav')
     x_freq, y_freq = show_freq_domain('asset/new_record/newhappy.wav')
-    
-   
-    
-    # plt.figure(figsize=(40, 20))
-    plt.subplot(3, 1, 1)
-    plt.plot(x_time, y_time)
-    # plt.plot(sliced_time, sliced_normalized_wave_data)
-    plt.title('Time Domain')
-    plt.xlabel('Time')
-    plt.ylabel('Normalised amplitudes')
-    plt.subplot(3, 1, 2)
-    plt.plot(x_freq, y_freq)
-    plt.title('Frequency Domain')
-    plt.xlabel('Frequency')
-    plt.ylabel('')
-
-    
-    
-    enhance(x_freq,50,100)
-
-    x_freq, y_freq = show_freq_domain('enhance.wav')
-    plt.subplot(3, 1, 3)
-    plt.plot(x_freq, y_freq)
-    plt.title('After enhance')
-    plt.xlabel('Frequency')
-    plt.ylabel('')
-    plt.show() 
-    
+    enhance(x_freq, 0, 2)
 

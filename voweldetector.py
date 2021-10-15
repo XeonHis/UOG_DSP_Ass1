008238b1607f630ef772187e518620ece11da846
Paul_Mac <yingheting@gmail.com>
2021-10-15 12:29:12 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- voweldetector.py
+++ voweldetector.py
@@ -6,39 +6,43 @@
 # frequency of E: 126Hz
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 from file_process import *
 
 
+# todo: 分段成小slot进行fft
 def detect_original_frequency(file_path):
     wave_data, nchannels, sample_width, framerate, numframes = read_file(file_path)
 
-    # print(wave_data)
     abs_fft = np.abs(np.fft.fft(wave_data))
     normalized_abs_fft = abs_fft / len(wave_data)
     half_fft = 2 * normalized_abs_fft[range(int(len(wave_data) / 2))]
     freqs = np.linspace(0, framerate, numframes)
 
-    xorder = np.argsort(-half_fft)
-    # print('xorder = ', xorder)
-    xworder = list()
-    for x in xorder:
-        xworder.append(freqs[x])
-    fworder = list()
-    for x in xorder:
-        fworder.append(half_fft[x])
-
-    # plt.plot(freqs[:int(len(freqs) / 2)], half_fft)
-    #
-    # plt.title('Frequency Domain')
-    # plt.xlabel('Frequency')
-    # plt.ylabel('')
-    # plt.show()
     return freqs[:int(len(freqs) / 2)], 20 * np.log10(half_fft / np.max(half_fft))
 
 
 if __name__ == '__main__':
-    sen_time, sen_fft = detect_original_frequency('asset/new_record/sentence.wav')
+    sen_time, sen_fft = detect_original_frequency('asset/new_record/pi.wav')
+    ai_time, ai_fft = detect_original_frequency('asset/sound_æ.wav')
+    i_time, i_fft = detect_original_frequency('asset/new_record/i.wav')
+    wu_time, wu_fft = detect_original_frequency('asset/new_record/wu.wav')
+    u_time, u_fft = detect_original_frequency('asset/new_record/u.wav')
 
+    plt.subplot(3, 2, 1)
+    plt.plot(sen_time, sen_fft)
+
+    plt.subplot(3, 2, 2)
+    plt.plot(ai_time, ai_fft)
+
+    plt.subplot(3, 2, 3)
+    plt.plot(i_time, i_fft)
+
+    plt.subplot(3, 2, 4)
+    plt.plot(wu_time, wu_fft)
+
+    plt.subplot(3, 2, 5)
+    plt.plot(u_time, u_fft)
 
     plt.show()

f2e0aded4a9c57872eed90ffb93bbdc321fa41cf
Paul_Mac <yingheting@gmail.com>
2021-10-15 19:01:28 +0100

    detector completed

--- voweldetector.py
+++ voweldetector.py
@@ -7,86 +7,107 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import os
 
 from file_process import *
 
 
 def divide_wav_file(file_path):
-    wave_data, nchannels, sample_width, framerate, numframes = read_file(file_path)
-
-    time_slot = 1
-    slot_frames = int(time_slot * framerate)
-    start = 0
-    # print(math.ceil(nframes / slot_frames))
-    if nchannels > 1:
-        wave_data.shape = -1, 2
-        wave_data = wave_data.T
-        # temp_data = wave_data.T
-    else:
-        wave_data = wave_data.T
-        # temp_data = wave_data.T
-
-    for i in range(math.ceil(numframes / slot_frames)):
-        num = start + slot_frames
-        print(num)
-        if (num) < numframes:
-            print(wave_data[start], wave_data[start + slot_frames - 1])
-            current_wave_data = wave_data[start:start + slot_frames]
-        else:
-            print(wave_data[start], wave_data[numframes - 1])
-            current_wave_data = wave_data[start:numframes]
-
-        chunkAudio = wave.open('temp/slot_' + str(i) + '.wav', 'w')
-        chunkAudio.setnchannels(nchannels)
-        chunkAudio.setsampwidth(sample_width)
-        chunkAudio.setframerate(framerate)
-        chunkAudio.writeframes(current_wave_data)
-        chunkAudio.close()
-
-        start = start + slot_frames
+    pass
 
 
 def detect_original_frequency(file_path):
     # 74752, 1, 2, 44100, 74752
     wave_data, nchannels, sample_width, framerate, numframes = read_file(file_path)
-    # todo: 分段成小slot进行fft
 
-    # time_slot = 0.02
-    # slot_frames = int(time_slot * framerate)
-    # start = 0
-    # for i in range(math.ceil(numframes / slot_frames)):
-    #     if start + slot_frames < numframes:
-    #         current_wave_data = wave_data[start:start + slot_frames]
-    #     else:
-    #         current_wave_data = wave_data[start:numframes]
-    #     current_abs_fft = np.abs((np.fft.fft(current_wave_data)))
-    #     current_normalized_fft = current_abs_fft / len(current_wave_data)
-    #     half_fft = 2 * current_normalized_fft[range(int(len(current_wave_data) / 2))]
-    #     freqs = np.linspace(0, framerate, slot_frames)
-    #
-    #     # slot = np.fft.ifft(cur)
-    #     # clr = np.real(enhance)
-    #     # audio = clr.astype(np.int16)
-    #     # wavfile.write('slot_' + str(i) + '.wav', framerate, current_wave_data)
-    #
-    #     start = start + slot_frames
-    #
-    #     # plt.plot(freqs[:int(len(freqs) / 2)], half_fft)
-    #     max_wave_idx = np.argmax(half_fft)
-    #     freqs = freqs[:int(len(freqs) / 2)]
-    #     max_wave_freq = freqs[int(max_wave_idx)]
-    #     print('idx:', max_wave_idx, '  freq:', max_wave_freq)
+    abs_fft = np.abs(np.fft.fft(wave_data))
+    normalized_abs_fft = abs_fft / len(wave_data)
+    half_fft = 2 * normalized_abs_fft[range(int(len(wave_data) / 2))]
+    freqs = np.linspace(0, framerate, numframes)
 
-    # abs_fft = np.abs(np.fft.fft(wave_data))
-    # normalized_abs_fft = abs_fft / len(wave_data)
-    # half_fft = 2 * normalized_abs_fft[range(int(len(wave_data) / 2))]
-    # freqs = np.linspace(0, framerate, numframes)
-    #
-    # return freqs[:int(len(freqs) / 2)], 20 * np.log10(half_fft / np.max(half_fft))
+    return freqs[:int(len(freqs) / 2)], 20 * np.log10(half_fft / np.max(half_fft))
 
 
 if __name__ == '__main__':
-    divide_wav_file('asset/new_record/newhappy.wav')
-    # detect_original_frequency('asset/new_record/moment.wav')
+    divide_wav_file(None)
+
+    path = r"temp"
+    files = os.listdir(path)
+    files = [path + "/" + f for f in files if f.endswith('.wav')]
+    # print(len(files))
+    sen_freqs = list()
+    for i in range(len(files)):
+        time_i, slot_fft_i = detect_original_frequency(files[i])
+        # print(time_i[int(np.argmax(slot_fft_i))])
+        sen_freqs.append(time_i[int(np.argmax(slot_fft_i))])
+    sen_freqs.sort()
+    print(sen_freqs)
+
+    vowel_freqs = dict()
+    time_1, slot_fft_1 = detect_original_frequency('asset/new_record/e.wav')
+    time_2, slot_fft_2 = detect_original_frequency('asset/new_record/i.wav')
+    time_3, slot_fft_3 = detect_original_frequency('asset/new_record/er.wav')
+    time_4, slot_fft_4 = detect_original_frequency('asset/new_record/wu.wav')
+    time_5, slot_fft_5 = detect_original_frequency('asset/new_record/u.wav')
+    time_6, slot_fft_6 = detect_original_frequency('asset/new_record/o.wav')
+
+    vowel_freqs['e'] = (time_1[int(np.argmax(slot_fft_1))])
+    vowel_freqs['i'] = (time_2[int(np.argmax(slot_fft_2))])
+    vowel_freqs['er'] = (time_3[int(np.argmax(slot_fft_3))])
+    vowel_freqs['wu'] = (time_4[int(np.argmax(slot_fft_4))])
+    vowel_freqs['u'] = (time_5[int(np.argmax(slot_fft_5))])
+    vowel_freqs['o'] = (time_6[int(np.argmax(slot_fft_6))])
+
+    print(vowel_freqs)
+
+    scores = dict()
+
+    for k, v, in vowel_freqs.items():
+        temp_list = list()
+        for idx in range(len(sen_freqs)):
+            value = sen_freqs[idx] / v
+            if value > 1.3:
+                value = math.modf(value)[0]
+            temp_list.append(value)
+
+        for i in range(len(temp_list)):
+            if temp_list[i] < 1:
+                temp_list[i] = 1 - temp_list[i]
+            else:
+                temp_list[i] = temp_list[i] - 1
+        temp_list.sort(reverse=False)
+        scores[k] = temp_list[0]
+
+    print('score(distance to 100%): \n', scores)
+
+    plt.subplot(2, 3, 1)
+    plt.plot(time_1, slot_fft_1)
+    plt.title('e')
+    plt.xlim(0, 600)
+
+    plt.subplot(2, 3, 2)
+    plt.plot(time_2, slot_fft_2)
+    plt.title('i')
+    plt.xlim(0, 600)
+
+    plt.subplot(2, 3, 3)
+    plt.plot(time_3, slot_fft_3)
+    plt.title('er')
+    plt.xlim(0, 600)
+
+    plt.subplot(2, 3, 4)
+    plt.plot(time_4, slot_fft_4)
+    plt.title('wu')
+    plt.xlim(0, 600)
+
+    plt.subplot(2, 3, 5)
+    plt.plot(time_5, slot_fft_5)
+    plt.title('u')
+    plt.xlim(0, 600)
+
+    plt.subplot(2, 3, 6)
+    plt.plot(time_6, slot_fft_6)
+    plt.title('o')
+    plt.xlim(0, 600)
 
     plt.show()
